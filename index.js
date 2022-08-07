2097efb0cee8cedabd73a66acdd76855285757e3
SKumar <sk.kumar561@gmail.com>
2022-08-07 19:51:39 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/dbname")
-    .then(() => console.log('Mongodb is connected'))
+const url = "mongodb://localhost:27017/dbname"
+
+new MongoClient(url).connect()
+    .then(() => console.log("Mongodb is connected"))
     .catch(err => console.log(err))
+
 
 
 app.listen(8000, () => {
