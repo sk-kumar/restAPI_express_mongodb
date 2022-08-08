90246d966c1a9ea0b7edb52d2c55a2063923bdb9
SKumar <sk.kumar561@gmail.com>
2022-08-08 14:05:47 +0530

    first commit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -8,11 +8,18 @@ const createProduct = async (req, res) => {
 
 // GET by query params
 const searchProduct = async (req, res) => {
-    const { title, price, availableSizes, color } = req.query
-    // 
-    let oneFilter = { $or: [{ title: title }, { price: price }, { availableSizes: availableSizes }, { color: color }] }
-    let twoFilters = { $and: [{ title: title }, { color: color }] }
-    let threeFilters = { $and: [{ title: title }, { color: color }, { $and: [{ price: { $gt: 1000 } }, { price: { $lt: 5000 } }] }] }
-    
-    let products = await productModel.find(oneFilter)
+    try {
+        const { title, price, availableSizes, color } = req.query
+        // 
+        let oneFilter = { $or: [{ title: title }, { price: price }, { availableSizes: availableSizes }, { color: color }] }
+        let twoFilters = { $and: [{ title: title }, { color: color }] }
+        let threeFilters = { $and: [{ title: title }, { color: color }, { $and: [{ price: { $gt: 1000 } }, { price: { $lt: 5000 } }] }] }
+
+        let products = await productModel.find(oneFilter)
+        res.status(200).send({ status: true, data: products });
+    } catch (error) {
+        res.status(200).send({ status: false, message: error.message });
+    }
 }
+
+module.exports = { createProduct, searchProduct }
